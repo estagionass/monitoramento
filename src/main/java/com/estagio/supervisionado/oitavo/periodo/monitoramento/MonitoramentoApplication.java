2e9defe4840a87768203c4134d8255685a358875
estagionass <72395569+estagionass@users.noreply.github.com>
2020-11-14 21:10:32 -0300

    Criação do projeto

---
+++ src/main/java/com/estagio/supervisionado/oitavo/periodo/monitoramento/MonitoramentoApplication.java
@@ -0,0 +1,13 @@
+package com.estagio.supervisionado.oitavo.periodo.monitoramento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoramentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoramentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estagio/supervisionado/oitavo/periodo/monitoramento/MonitoramentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estagio.supervisionado.oitavo.periodo.monitoramento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoramentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
