27ff56f5854f782ddf33a857c98fff6bf7dc5c4f
Engenharia da Computação 2017.1 Uninassau Graças Recife-PE <72395569+estagionass@users.noreply.github.com>
2020-11-16 23:32:47 -0300

    S

--- src/main/java/com/estagio/supervisionado/oitavo/periodo/monitoramento/repositories/MonitoramentoRepository.java
+++ src/main/java/com/estagio/supervisionado/oitavo/periodo/monitoramento/repositories/MonitoramentoRepository.java
@@ -1,8 +1,7 @@
 package com.estagio.supervisionado.oitavo.periodo.monitoramento.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.estagio.supervisionado.oitavo.periodo.monitoramento.domain.Monitoramento;
@@ -10,6 +9,6 @@ import com.estagio.supervisionado.oitavo.periodo.monitoramento.domain.Monitorame
 @Repository
 public interface MonitoramentoRepository extends JpaRepository<Monitoramento, Integer>{
 	
-	//Método reponsável por retornar uma lista de todos os objetos ordenados pelo id e ordenados de forma decrescente
-	public List<Monitoramento> findAllByOrderByIdDesc();
+	@Query(nativeQuery = true, value = "SELECT * FROM `tbl_clinica_dados` ORDER BY `id` DESC LIMIT 1")
+	Monitoramento findLastData();
 }
